ba10fbc0dfc68f91f0d95103d2be26da31249102
nn4343 <gea01441@nifty.com>
2018-08-12 15:17:25 +0900

    m00追加

---
+++ m00/my-app/src/app/statesrc/testControl.ts
@@ -0,0 +1,61 @@
+﻿//  psggConverterLib.dll converted from testControl.xlsx. 
+import { AppComponent } from '../app.component'; // add to template-source.txt
+import { testControlSub } from './testControlSub';
+
+export class testControl extends testControlSub {
+
+    public start() {
+        this.Goto(this.S_START);
+    }
+    public is_end() {
+        return this.CheckState(this.S_END);
+    }
+
+
+    /*
+        S_START
+    */
+    S_START(bFirst: boolean) {
+        if (bFirst) {
+            this.curstatename = 'S_START';
+            // this.curstatecmt  = '';
+        }
+        if (!this.HasNextState()) {
+            this.SetNext(this.S_HELLO);
+        }
+        if (this.HasNextState()) {
+            this.GoNext();
+        }
+    }
+    /*
+        S_END
+    */
+    S_END(bFirst: boolean) {
+        if (bFirst) {
+            this.curstatename = 'S_END';
+            // this.curstatecmt  = '';
+        }
+        if (this.HasNextState()) {
+            this.GoNext();
+        }
+    }
+    /*
+        S_HELLO
+        say hello!
+    */
+    S_HELLO(bFirst: boolean) {
+        if (bFirst) {
+            this.curstatename = 'S_HELLO';
+            // this.curstatecmt  = 'say hello!';
+            AppComponent.s_title='SYN-G-GEN !!!';
+        }
+        if (!this.HasNextState()) {
+            this.SetNext(this.S_END);
+        }
+        if (this.HasNextState()) {
+            this.GoNext();
+        }
+    }
+
+}
+
